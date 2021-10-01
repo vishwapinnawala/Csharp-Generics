6dbefe151859145de7b37589eaf788f59b205832
Vishwa Pinnawala <45973257+vishwapinnawala@users.noreply.github.com>
2021-10-01 20:16:19 +0530

    Add files via upload

---
+++ Generics/Gentest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+   public class Gentest<T>
+    {
+       private T data;
+
+       public T value
+       {
+           set
+           {
+             data = value;
+           }
+           get
+           {
+            return data;
+           }       
+       }
+    }
+}
